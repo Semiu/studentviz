2e91ee44740e5d53e08bba712cc7bcd80672e567
Semiu <ayobami.sm@gmail.com>
2020-07-26 20:17:15 -0500

    setting landing page

--- app.js
+++ app.js
@@ -4,13 +4,6 @@ const path = require('path');
 //Load the Express module
 const express = require('express');
 
-//Load the Handle Bars module
-const hbs = require('hbs');
-
-const partialPath = path.join(__dirname, 'views/partial');
-
-hbs.registerPartials(partialPath);
-
 //Initialize the app from the express module
 const app = express();
 
@@ -19,33 +12,6 @@ const publicAsset = path.join(__dirname, 'public');
 
 app.use(express.static(publicAsset));
 
-//
-const viewPath = path.join(__dirname, 'views/full');
-app.set('views', viewPath);
-
-//
-app.set('view engine', 'hbs')
-
-//Getting assets created in the app
-app.get('', (req, res) => {
-    res.send("Load my Man!")
-
-});
-
-app.get('/motionchart', (req, res) => {
-    res.send("Motion chart")
-
-});
-
-app.get('/dashboard', (req, res) => {
-    res.send("Dashboard")
-
-});
-
-app.get('/about', (req, res) => {
-    res.send("About")
-
-});
 
 //Assigning the port number
 const port = process.env.PORT || 3000;
